8a1ed0329f05608b8706feae099c7666c2cb3a8d
surjendu104 <cyber00daddy@gmail.com>
2022-09-11 20:24:13 +0530

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Inventory-routing-problem-with-modified-Any-Colony-Algorithm-
+# Inventory-routing-problem-with-modified-Any-Colony-Algorithm
+This is implepentation of this [Research Paper](https://drive.google.com/file/d/1zA70COwq458tK2xrlB3gcoE5LGdFI3B-/view?usp=sharing)
+
+### Quick Guide
+1. Open `git bash` and paste and clone the repositoryn  
+```git clone https://github.com/surjendu104/Inventory-routing-problem-with-modified-Any-Colony-Algorithm-.git```
+
+2. Open the file in terminal
+3. Run  
+```py plot_result.py```
+
+### Output
+Output will be store in `output.txt` and plotting result will be shown in  `output.png`  
+The plot is of `Number of Iteration` vs `Minimum Distance`
+Here's one sample Plot Result 
+
+  
+![output.png](sample.png)
--- plot_result.py
+++ plot_result.py
@@ -1,22 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from aco_imp import track_min_distance,gen_number
+from aco_imp import track_min_distance, gen_number
 
-generation_array =[]
-for i in range(1,gen_number+1):
+generation_array = []
+for i in range(1, gen_number+1):
     generation_array.append(i)
 
-# plt.rcParams["figure.figsize"] = [7.50, 3.50]
-# plt.rcParams["figure.autolayout"] = True
 
-# print(track_min_distance)
-# print(generation_array)
-
-x=np.array(track_min_distance)
-y=np.array(generation_array)
+x = np.array(track_min_distance)
+y = np.array(generation_array)
 
 plt.title("Change of Minimum distance - Iteration")
 plt.xlabel("Number of Iteration")
 plt.ylabel("Minimum Distance")
-plt.plot(y,x,color="blue")
+plt.plot(y, x, color="blue")
 plt.savefig("output.png")
